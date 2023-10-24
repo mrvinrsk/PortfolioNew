ca398777a3f83b9f78c43429243671cb8e22c9f3
Semikuhl <marvinrosskothen@proton.me>
2023-10-24 23:16:05 +0200

    Initial commit

---
+++ about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Über mich | Marvin Roßkothen</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <script defer src="js/smooth-scrollbar.js"></script>
+    <script defer src="js/swup.umd.js"></script>
+    <script defer src="js/config.js"></script>
+</head>
+<body>
+
+<?php include 'php/elements/navigation.inc'; ?>
+
+<main id="swup" class="transition-fade">
+    <h2>About</h2>
+</main>
+
+</body>
+</html>
---
+++ js/config.js
@@ -0,0 +1,32 @@
+const swup = new Swup();
+
+const hamburger = document.getElementById('hamburger');
+const nav = document.querySelector('nav');
+
+nav.querySelectorAll('.slide-link').forEach(link => {
+    link.dataset.word = link.textContent;
+    link.textContent = '';
+
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+
+        nav.querySelectorAll('.slide-link').forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
+    });
+
+    // check if href is the current page
+    if (link.href === window.location.href) {
+        link.classList.add('active');
+    }
+});
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    nav.classList.toggle('open');
+});
+
+window.Scrollbar.init(document.querySelector('#swup'), {
+    damping: 0.05,
+    renderByPixel: true,
+});
